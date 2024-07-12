fd9187e05e0b87be6311f5dd479dc1c5f6fa0070
OhMinsSup <mins5190@gmail.com>
2024-07-12 11:14:05 +0900

    add: [remix]: userUser Hook 추가

---
+++ apps/website-remix/app/hooks/useUser.tsx
@@ -0,0 +1,32 @@
+import type { SerializeFrom } from "@remix-run/node";
+import { useRouteLoaderData } from "@remix-run/react";
+
+import type { RoutesLoaderData } from "~/.server/routes/root/root.loader";
+
+function isUser(
+  user: SerializeFrom<RoutesLoaderData>["user"],
+): user is SerializeFrom<RoutesLoaderData>["user"] {
+  if (typeof user === "undefined" || user === null) {
+    return false;
+  }
+
+  return typeof user === "object" && typeof user.id === "string";
+}
+
+export function useOptionalUser() {
+  const data = useRouteLoaderData<RoutesLoaderData>("root");
+  if (!data || !isUser(data.user)) {
+    return undefined;
+  }
+  return data.user;
+}
+
+export function useUser() {
+  const maybeUser = useOptionalUser();
+  if (!maybeUser) {
+    throw new Error(
+      "No user found in root loader, but user is required by useUser. If user is optional, try useOptionalUser instead.",
+    );
+  }
+  return maybeUser;
+}
