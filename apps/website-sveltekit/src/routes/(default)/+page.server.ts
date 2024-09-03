f20afe8ce4fa1df39af2f479583c8da147bc9d5b
OhMinSeop <mins5190@gmail.com>
2024-09-03 14:41:29 +0900

    add: [sveltekit] trpc 제거

--- apps/website-sveltekit/src/routes/(default)/+page.server.ts
+++ apps/website-sveltekit/src/routes/(default)/+page.server.ts
@@ -1,13 +1,7 @@
-import { initTRPCSSRClient } from "$lib/trpc/server";
-
 import type { PageServerLoadEvent } from "./$types";
 
 export async function load(event: PageServerLoadEvent) {
-  // const trpcClient = initTRPCSSRClient(event.request.headers, event.setHeaders);
-
-  // const message = await trpcClient.etc.hello.query();
-
   return {
-    // message,
+    message: "ok",
   };
 }
