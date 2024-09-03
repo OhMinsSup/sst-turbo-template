095f3bb3e5eb87313d354ad6a94890fae5872ab5
OhMinSeop <mins5190@gmail.com>
2024-09-03 15:01:56 +0900

    add: [etc] tsconfig, utils 수정

--- packages/sdk/tsup.config.ts
+++ packages/sdk/tsup.config.ts
@@ -1,17 +1,15 @@
 import type { Options } from "tsup";
 import { defineConfig } from "tsup";
 
-export default defineConfig((options: Options) => [
-  {
-    entry: {
-      index: "src/index.ts",
-      auth: "src/auth/index.ts",
-    },
-    format: ["esm", "cjs"],
-    dts: true,
-    minify: !options.watch,
-    minifyWhitespace: true,
-    clean: true,
-    ...options,
+export default defineConfig((options: Options) => ({
+  entry: {
+    index: "src/index.ts",
+    auth: "src/auth/index.ts",
   },
-]);
+  format: ["esm", "cjs"],
+  dts: true,
+  minify: !options.watch,
+  minifyWhitespace: true,
+  clean: true,
+  ...options,
+}));
--- tooling/tailwind/base.ts
+++ tooling/tailwind/base.ts
@@ -10,13 +10,6 @@ export default {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
