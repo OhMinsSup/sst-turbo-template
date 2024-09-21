8851a6637603ce33845919207c7e633e9585f4cd
OhMinSeop <mins5190@gmail.com>
2024-09-21 22:49:05 +0900

    add: [remix] 헤더구현

--- apps/website-remix/app/routes/_index.tsx
+++ apps/website-remix/app/routes/_index.tsx
@@ -1,45 +1,52 @@
+import { SiteHeader } from "~/components/shared/SiteHeader";
+
 export { loader } from "~/.server/routes/index/index.loader";
 
 export default function Index() {
   return (
-    <div className="p-4 font-sans">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <p className="mt-4">
-        Remix is a full-stack web framework for React. It's designed to make
-        building production-ready applications faster and easier.
-      </p>
-      <ul className="mt-4 list-disc space-y-2 pl-6">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <SiteHeader />
+      <main className="container max-w-[620px] px-4 sm:px-6">
+        <div className="p-4 font-sans">
+          <h1 className="text-3xl">Welcome to Remix</h1>
+          <p className="mt-4">
+            Remix is a full-stack web framework for React. It's designed to make
+            building production-ready applications faster and easier.
+          </p>
+          <ul className="mt-4 list-disc space-y-2 pl-6">
+            <li>
+              <a
+                className="text-blue-700 underline visited:text-purple-900"
+                target="_blank"
+                href="https://remix.run/start/quickstart"
+                rel="noreferrer"
+              >
+                5m Quick Start
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-blue-700 underline visited:text-purple-900"
+                target="_blank"
+                href="https://remix.run/start/tutorial"
+                rel="noreferrer"
+              >
+                30m Tutorial
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-blue-700 underline visited:text-purple-900"
+                target="_blank"
+                href="https://remix.run/docs"
+                rel="noreferrer"
+              >
+                Remix Docs
+              </a>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </>
   );
 }
