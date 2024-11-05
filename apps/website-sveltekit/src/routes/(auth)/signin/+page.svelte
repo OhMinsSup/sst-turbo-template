dbed0dfb83cf85829433a15cd62c340aeaf31520
OhMinSeop <mins5190@gmail.com>
2024-11-05 15:55:57 +0900

    add: [sveltekit, solid] 프로젝트 삭제

--- apps/website-sveltekit/src/routes/(auth)/signin/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { SignInForm } from "$lib/components/auth/SignInForm";
-
-  import type { PageData } from "./$types";
-
-  export let data: PageData;
-</script>
-
-<SignInForm {data} />
